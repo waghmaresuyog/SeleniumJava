3378ac77ff71cb0892833cb0901000b4ad83b0f2
Suyog Waghmare <suyog.waghmare@encora.com>
2023-09-25 20:04:36 +0530

    update code

--- SeleniumJavaCode/src/test/java/excleOperactions/WriteExcelFile.java
+++ SeleniumJavaCode/src/test/java/excleOperactions/WriteExcelFile.java
@@ -5,8 +5,6 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
